6f7b09f650d6e24a4c99630fda25129141e3e83f
Mark Allender <mark.allender@gmail.com>
2016-06-30 11:52:51 -0500

    add second disk drive

--- 6502/disk.cpp
+++ 6502/disk.cpp
@@ -47,7 +47,9 @@ SOFTWARE.
 // I NEED TO FIGURE OUT WHAT THIS DEFINE WORKS
 #define NIBBLES_PER_TRACK 0x1A00
 
-disk_drive drive_1;
+static disk_drive drive_1;
+static disk_drive drive_2;
+static disk_drive *Current_drive;
 
 void disk_drive::init()
 {
@@ -83,11 +85,10 @@ void disk_drive::readwrite()
 		m_data_buffer = m_track_data[m_current_byte];
 #if defined(LOG_DISK)
 		LOG(INFO) << "Read: " << std::setw(4) << std::setfill(' ') << std::setbase(16) << m_current_byte << " " <<
-			std::setw(2) << std::setbase(16) << (uint32_t)m_data_buffer;
+std::setw(2) << std::setbase(16) << (uint32_t)m_data_buffer;
 #endif
-	}
-	else {
-		// this is write mode
+	} else {
+	// this is write mode
 	}
 	m_current_byte++;
 	if (m_current_byte == m_track_size) {
@@ -109,9 +110,9 @@ void disk_drive::set_new_track(const uint8_t track)
 // inserts a disk image into the given slot
 bool disk_insert(const char *disk_image_filename, const uint32_t slot)
 {
-	drive_1.m_disk_image = new disk_image();
-	drive_1.m_disk_image->init();
-	drive_1.m_disk_image->load_image(disk_image_filename);
+	Current_drive->m_disk_image = new disk_image();
+	Current_drive->m_disk_image->init();
+	Current_drive->m_disk_image->load_image(disk_image_filename);
 	return true;
 }
 
@@ -129,65 +130,70 @@ uint8_t read_handler(uint16_t addr)
 	case 0x5:
 	case 0x6:
 	case 0x7:
-		{
-			if ((addr & 0x000f) <= 0x7) {
-				uint8_t phase = (addr >> 1) & 3;	   // gets which motor, 0, 1, 2, or 3
+	{
+		if ((addr & 0x000f) <= 0x7) {
+			uint8_t phase = (addr >> 1) & 3;	   // gets which motor, 0, 1, 2, or 3
 
-				if (addr & 1) {
-					drive_1.m_phase_status |= (1<<phase);
-				} else {
-					drive_1.m_phase_status &= ~(1<<phase);
-				}
+			if (addr & 1) {
+				Current_drive->m_phase_status |= (1 << phase);
+			} else {
+				Current_drive->m_phase_status &= ~(1 << phase);
+			}
 
-				// step in (increment) to new track.  phase 1, then phase 2
-				// activated from even numbered track.  Phase 3 then phase 0
-				// activated from odd numbered track.
-				//
-				// step out (decrement) to new track.  Phase 3, then phase
-				// 2 from even numbered track.  Phase 1, then phase 0
-				// from odd numbered track.
-				int dir = 0;
-				if (drive_1.m_phase_status & (1 << ((drive_1.m_half_track_count + 1) & 3))) {
-					dir = 1;
-				}
-				if (drive_1.m_phase_status & (1 << ((drive_1.m_half_track_count + 3) & 3))) {
-					dir = -1;
-				}
+			// step in (increment) to new track.  phase 1, then phase 2
+			// activated from even numbered track.  Phase 3 then phase 0
+			// activated from odd numbered track.
+			//
+			// step out (decrement) to new track.  Phase 3, then phase
+			// 2 from even numbered track.  Phase 1, then phase 0
+			// from odd numbered track.
+			int dir = 0;
+			if (Current_drive->m_phase_status & (1 << ((Current_drive->m_half_track_count + 1) & 3))) {
+				dir = 1;
+			}
+			if (Current_drive->m_phase_status & (1 << ((Current_drive->m_half_track_count + 3) & 3))) {
+				dir = -1;
+			}
 
-				if (dir != 0) {
-					drive_1.m_half_track_count = std::max(0, std::min(79, drive_1.m_half_track_count + dir));
-					auto new_track = std::min(drive_1.m_disk_image->m_num_tracks - 1, drive_1.m_half_track_count >> 1);
-					if (new_track != drive_1.m_current_track) {
-						drive_1.set_new_track(new_track);
-					}
-				}
-#if defined(LOG_DISK)
-				LOG(INFO) << "phases: " << std::setw(1) << ((drive_1.m_phase_status >> 3) & 0x1)
-												<< std::setw(1) << ((drive_1.m_phase_status >> 2) & 0x1)
-												<< std::setw(1) << ((drive_1.m_phase_status >> 1) & 0x1) 
-												<< std::setw(1) << (drive_1.m_phase_status & 0x1)
-							 << " track: " << std::setw(2) << (int)drive_1.m_half_track_count
-							 << " dir: " << std::setw(1) << dir
-							 << " addr: " << std::setbase(16) << (addr & 0xff);
-#endif
+			if (dir != 0) {
+				Current_drive->m_half_track_count = std::max(0, std::min(79, Current_drive->m_half_track_count + dir));
+				auto new_track = std::min(Current_drive->m_disk_image->m_num_tracks - 1, Current_drive->m_half_track_count >> 1);
+				if (new_track != Current_drive->m_current_track) {
+					Current_drive->set_new_track(new_track);
 				}
 			}
-		break;
+#if defined(LOG_DISK)
+			LOG(INFO) << "phases: " << std::setw(1) << ((Current_drive->m_phase_status >> 3) & 0x1)
+				<< std::setw(1) << ((Current_drive->m_phase_status >> 2) & 0x1)
+				<< std::setw(1) << ((Current_drive->m_phase_status >> 1) & 0x1)
+				<< std::setw(1) << (Current_drive->m_phase_status & 0x1)
+				<< " track: " << std::setw(2) << (int)Current_drive->m_half_track_count
+				<< " dir: " << std::setw(1) << dir
+				<< " addr: " << std::setbase(16) << (addr & 0xff);
+#endif
+		}
+	}
+	break;
 
 	case 0x8:
 	case 0x9:
 		// turn the motor on or off
-		drive_1.m_motor_on = (addr & 1);
+		Current_drive->m_motor_on = (addr & 1);
 		break;
 
 	case 0xa:
 	case 0xb:
 		// pick a specific drive
+		if (((addr & 0xf) - 0xa) == 0) {
+			Current_drive = &drive_1;
+		} else {
+			Current_drive = &drive_2;
+		}
 		//LOG(INFO) << "Select drive: " << ((addr & 0xf) - 0x9);
 		break;
 
 	case 0xc:
-		drive_1.readwrite();
+		Current_drive->readwrite();
 		break;
 		
 	case 0xd:
@@ -195,16 +201,16 @@ uint8_t read_handler(uint16_t addr)
 		break;
 
 	case 0xe:
-		drive_1.m_write_mode = false;
+		Current_drive->m_write_mode = false;
 		break;
 
 	case 0xf:
-		drive_1.m_write_mode = true;
+		Current_drive->m_write_mode = true;
 		break;
 	}
 
 	if (!(addr & 0x1)) {
-		return drive_1.m_data_buffer;
+		return Current_drive->m_data_buffer;
 	} else {
 		return 0;
 	}
@@ -220,4 +226,6 @@ void disk_init(memory &mem)
 {
 	mem.register_slot_handler(6, read_handler, write_handler);
 	drive_1.init();
+	drive_2.init();
+	Current_drive = &drive_1;
 }
